84e194d5053c337b272d86c40a0937f409100952
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-10-18 15:23:34 +0000

    created framework to create and load topologies in external file

---
+++ src/create_topologies.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+"""
+In this file we create a few sample network topologies for testing
+
+"""
+
+topology = np.array([[0, 1, 1], [1, 0, 0], [1, 0, 0]])
+np.savetxt("topology/topo_1.txt", topology, fmt="%d")

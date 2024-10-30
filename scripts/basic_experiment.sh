b090ede671f4a8dc133e2caa59d4bf7922797f8e
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-10-30 23:05:59 +0000

    reconfigured expeirment to consider complete graphs

--- scripts/basic_experiment.sh
+++ scripts/basic_experiment.sh
@@ -10,7 +10,7 @@ do
         do
             for label_alpha in 100 1000;
             do
-                for topo in 1 2 3 4 5;
+                for topo in 1 2 3 4 5 6 7;
                 do
                     topo_path=topology/topo_$topo.txt
                     python decentralized_main.py --topology $topo_path --aggregation_strategy $aggregation_strategy --sample_alpha $sample_alpha --label_alpha $label_alpha --prox_coeff $prox_coeff --rounds 10

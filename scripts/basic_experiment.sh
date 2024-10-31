00c60a659f811a49919b326edea69e7df2f61f1a
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-10-31 18:41:38 +0000

    adding figures

--- scripts/basic_experiment.sh
+++ scripts/basic_experiment.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/bin/bash 
+#PBS -l select=1
+#PBS -l walltime=01:00:00
+#PBS -q debug
+#PBS -l filesystems=home:eagle
+#PBS -A superbert
+#PBS -M sakarvadia@uchicago.edu
+
+cd /eagle/projects/argonne_tpc/mansisak/distributed_ml 
+module use /soft/modulefiles
+module load conda
+conda activate env/
 
 cd /eagle/projects/argonne_tpc/mansisak/distributed_ml/src 
 
@@ -10,10 +21,10 @@ do
         do
             for label_alpha in 100 1000;
             do
-                for topo in 1 2 3 4 5 6 7;
+                for topo in 7 6 5 4 3 2 1;
                 do
                     topo_path=topology/topo_$topo.txt
-                    python decentralized_main.py --topology $topo_path --aggregation_strategy $aggregation_strategy --sample_alpha $sample_alpha --label_alpha $label_alpha --prox_coeff $prox_coeff --rounds 10
+                    python decentralized_main.py --topology $topo_path --aggregation_strategy $aggregation_strategy --sample_alpha $sample_alpha --label_alpha $label_alpha --prox_coeff $prox_coeff --rounds 100
                 done
             done
         done

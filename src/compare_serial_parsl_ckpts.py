7ef3ef2afadd98d49e03aabacf55ef825d923ab7
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-11-04 21:25:24 +0000

    adding better printing

--- src/compare_serial_parsl_ckpts.py
+++ src/compare_serial_parsl_ckpts.py
@@ -7,6 +7,9 @@ def compare_models(sd_1, sd_2):
         if torch.equal(key_item_1[1].cpu(), key_item_2[1].cpu()):
             pass
         else:
+            # print(key_item_1[1])
+            # print(key_item_2[1])
+            # return 1
             models_differ += 1
             if key_item_1[0] == key_item_2[0]:
                 print("Mismtach found at", key_item_1[0])
@@ -16,24 +19,33 @@ def compare_models(sd_1, sd_2):
         print("Models match perfectly! :)")
         return 0
 
-    raise 1
+    return 1
 
 
 # iterate through models
 
-for round_idx in range(5):
-    print(f"{round_idx=}")
-    print("before local train")
-    sd_1 = torch.load(f"0_{round_idx}_before_local_train_parsl.pth", weights_only=True)
-    sd_2 = torch.load(f"0_{round_idx}_before_local_train_serial.pth", weights_only=True)
-    compare_models(sd_1, sd_2)
+for round_idx in range(2):
+    for model in range(2):
+        print(f"{round_idx=}, {model=}")
+        print("before local train")
+        sd_1 = torch.load(
+            f"{model}_{round_idx}_before_local_train_parsl.pth", weights_only=True
+        )
+        sd_2 = torch.load(
+            f"{model}_{round_idx}_before_local_train_serial.pth", weights_only=True
+        )
+        compare_models(sd_1, sd_2)
 
-    print("after local train")
-    sd_1 = torch.load(f"0_{round_idx}_after_local_train_parsl.pth", weights_only=True)
-    sd_2 = torch.load(f"0_{round_idx}_after_local_train_serial.pth", weights_only=True)
-    compare_models(sd_1, sd_2)
+        print("after local train")
+        sd_1 = torch.load(
+            f"{model}_{round_idx}_after_local_train_parsl.pth", weights_only=True
+        )
+        sd_2 = torch.load(
+            f"{model}_{round_idx}_after_local_train_serial.pth", weights_only=True
+        )
+        compare_models(sd_1, sd_2)
 
-    print("after aggregate")
-    sd_1 = torch.load(f"0_{round_idx}_after_agg_parsl.pth", weights_only=True)
-    sd_2 = torch.load(f"0_{round_idx}_after_agg_serial.pth", weights_only=True)
-    compare_models(sd_1, sd_2)
+        # print("after aggregate")
+        # sd_1 = torch.load(f"{model}_{round_idx}_after_agg_parsl.pth", weights_only=True)
+        # sd_2 = torch.load(f"{model}_{round_idx}_after_agg_serial.pth", weights_only=True)
+        # compare_models(sd_1, sd_2)

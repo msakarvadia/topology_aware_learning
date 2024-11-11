9b9cb005ee4b4c36d4f26d5d60f44ec9ad35e057
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-11-11 23:06:01 +0000

    testing script

---
+++ src/test_consistancy.py
@@ -0,0 +1,14 @@
+import os
+
+# flush any cached logs
+command = "rm logs/ -r"
+os.system(command)
+
+for i in range(5):
+    command = f"""
+    python decentralized_main.py  \
+        --prox_coeff 0 \
+        --aggregation_strategy unweighted \
+        --rounds 2 --out_dir logs/{i}/ \
+    """
+    os.system(command)

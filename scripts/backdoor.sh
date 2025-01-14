f42527c8f1c836c56964f978063b0cee3d7217af
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2025-01-14 00:43:50 +0000

    updated queue info

--- scripts/backdoor.sh
+++ scripts/backdoor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
-#PBS -l select=1
-#PBS -l walltime=00:05:00
-#PBS -q preemptable
+#PBS -l select=10
+#PBS -l walltime=03:00:00
+#PBS -q prod
 #PBS -l filesystems=home:eagle
 #PBS -A superbert
 #PBS -M sakarvadia@uchicago.edu
@@ -18,4 +18,4 @@ cd /eagle/projects/argonne_tpc/mansisak/distributed_ml/src/experiments
 NODES=`cat $PBS_NODEFILE | wc -l`
 echo '# of nodes =' $NODES
 
-python backdoor.py --round 10 --checkpoint_every 1 --num_nodes $NODES
+python backdoor.py --rounds 10 --checkpoint_every 1 --num_nodes $NODES

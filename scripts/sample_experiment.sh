5b8442d8d2bd362c8194ff6165de66c1b046a491
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2025-01-20 02:26:56 +0000

    added experimental scripts

--- scripts/sample_experiment.sh
+++ scripts/sample_experiment.sh
@@ -5,6 +5,7 @@
 #PBS -l filesystems=home:eagle
 #PBS -A superbert
 #PBS -M sakarvadia@uchicago.edu
+#PBS -N sample
 
 cd /eagle/projects/argonne_tpc/mansisak/distributed_ml 
 module use /soft/modulefiles
@@ -16,4 +17,4 @@ cd /eagle/projects/argonne_tpc/mansisak/distributed_ml/src/experiments
 NODES=`cat $PBS_NODEFILE | wc -l`
 echo '# of nodes =' $NODES
 
-python sample_experiment.py --rounds 300 --checkpoint_every 10 --num_nodes $NODES
+python sample_experiment.py --rounds 200 --checkpoint_every 10 --num_nodes $NODES

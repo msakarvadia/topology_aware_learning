603d828957d9c6bf50befabedda54c1f6a880ce1
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2025-01-29 16:46:05 +0000

    updaing experiment length

--- scripts/softmax.sh
+++ scripts/softmax.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
-#PBS -l select=10
-#PBS -l walltime=03:00:00
-#PBS -q prod
+#PBS -l select=2
+#PBS -l walltime=72:00:00
+#PBS -q preemptable
 #PBS -l filesystems=home:eagle
 #PBS -A superbert
 #PBS -M sakarvadia@uchicago.edu

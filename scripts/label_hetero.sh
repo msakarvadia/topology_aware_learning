8bc09d456d4cc7760fa34bcce16b9c24464f64f9
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2025-01-30 23:21:13 +0000

    adding label heterogeneity experiments in

---
+++ scripts/label_hetero.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#PBS -l select=1
+#PBS -l walltime=72:00:00
+#PBS -q preemptable
+#PBS -l filesystems=home:eagle
+#PBS -A superbert
+#PBS -M sakarvadia@uchicago.edu
+#PBS -N label_hetero
+#PBS -r y 
+
+cd /eagle/projects/argonne_tpc/mansisak/distributed_ml 
+module use /soft/modulefiles
+module load conda
+conda activate env/
+
+cd /eagle/projects/argonne_tpc/mansisak/distributed_ml/src/experiments
+
+NODES=`cat $PBS_NODEFILE | wc -l`
+echo '# of nodes =' $NODES
+
+python label_hetero.py --rounds 25 --checkpoint_every 5 --num_nodes $NODES

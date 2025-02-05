217274b35ee73b2a007fc7a1419604280a0097cb
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2025-02-05 20:49:34 +0000

    adding more refined backdoor experiments

--- src/experiments/backdoor.py
+++ src/experiments/backdoor.py
@@ -149,7 +149,8 @@ if __name__ == "__main__":
         if i % args.checkpoint_every == 0:
             print(f"running expeirment until round {i}")
             app_result_tuples = []
-            for softmax_coeff in [1, 10, 25, 50, 75, 100]:
+            for softmax_coeff in [1, 2, 4, 6, 8, 10]:
+                # for softmax_coeff in [1, 10, 25, 50, 75, 100]:
                 # iterate through aggregation strategies
                 for aggregation_strategy in [
                     "unweighted",

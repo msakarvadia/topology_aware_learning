8ff41dcc4dfe2a0b91e56734767fa3b8d763e5ff
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2025-02-11 22:28:09 +0000

    adding experiments for scheduler

---
+++ scripts/scheduler.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#PBS -l select=10
+#PBS -l walltime=03:00:00
+#PBS -q prod
+#PBS -l filesystems=home:eagle
+#PBS -A superbert
+#PBS -M sakarvadia@uchicago.edu
+#PBS -N backdoor
+#PBS -r y 
+
+cd /eagle/projects/argonne_tpc/mansisak/distributed_ml 
+module use /soft/modulefiles
+module load conda
+conda activate env/
+
+cd /eagle/projects/argonne_tpc/mansisak/distributed_ml/src/experiments
+
+NODES=`cat $PBS_NODEFILE | wc -l`
+echo '# of nodes =' $NODES
+
+python scheduler.py --rounds 30 --checkpoint_every 5 --num_nodes $NODES

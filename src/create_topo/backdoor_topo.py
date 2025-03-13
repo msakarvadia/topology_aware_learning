3b41f12aa3466920d233972159df019ca2c6499c
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2025-03-13 21:50:54 +0000

    adding template for stochastic block model graphs

--- src/create_topo/backdoor_topo.py
+++ src/create_topo/backdoor_topo.py
@@ -35,6 +35,7 @@ def mk_backdoor_topos(num_nodes=5) -> tuple[list[str], list[list[int]]]:
     # graphs = []
     graphs = {}
 
+    # BA
     g = nx.barabasi_albert_graph(n=33, m=1, seed=0)
     graphs["barabasi_albert_33_1"] = g
 
@@ -44,6 +45,39 @@ def mk_backdoor_topos(num_nodes=5) -> tuple[list[str], list[list[int]]]:
     g = nx.barabasi_albert_graph(n=33, m=3, seed=0)
     graphs["barabasi_albert_33_3"] = g
     # graphs.append(g)
+
+    # SB
+    """
+    sizes = [11, 11, 11]
+    self_conect = 0.5
+    community_connect = 0.009
+    probs = [[self_conect, community_connect, community_connect],
+              [community_connect, self_conect, community_connect],
+             [community_connect, community_connect, self_conect, ],
+             ]
+    g = nx.stochastic_block_model(sizes, probs, seed=0)
+    graphs["sb_11_05_0009"] = g
+
+    sizes = [11, 11, 11]
+    self_conect = 0.5
+    community_connect = 0.05
+    probs = [[self_conect, community_connect, community_connect],
+              [community_connect, self_conect, community_connect],
+             [community_connect, community_connect, self_conect, ],
+             ]
+    g = nx.stochastic_block_model(sizes, probs, seed=0)
+    graphs["sb_11_05_005"] = g
+
+    sizes = [11, 11, 11]
+    self_conect = 0.5
+    community_connect = 0.09
+    probs = [[self_conect, community_connect, community_connect],
+              [community_connect, self_conect, community_connect],
+             [community_connect, community_connect, self_conect, ],
+             ]
+    g = nx.stochastic_block_model(sizes, probs, seed=0)
+    graphs["sb_11_05_009"] = g
+    """
 
     """
     g = nx.barabasi_albert_graph(n=66, m=2, seed=0)

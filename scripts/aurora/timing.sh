623370ccee6ade3a3ccd50975d133ec0020b581a
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2025-05-11 19:14:54 +0000

    updating timing

--- scripts/aurora/timing.sh
+++ scripts/aurora/timing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #PBS -l select=1
-#PBS -l walltime=06:00:00
-#PBS -q prod
+#PBS -l walltime=01:00:00
+#PBS -q debug
 #PBS -l filesystems=home:flare
 #PBS -A AuroraGPT
 #PBS -M sakarvadia@uchicago.edu

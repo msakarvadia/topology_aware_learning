6f5a6e9654e9a0a62f5b9dd6cfecb6bba930c509
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2025-12-22 18:26:41 +0000

    reducing number of topos for heterogenous topologies

--- src/create_topo/test_topo.py
+++ src/create_topo/test_topo.py
@@ -41,7 +41,7 @@ def mk_test_topos(num_nodes=5, seed=0) -> tuple[list[str], list[list[int]]]:
         # 16,
         33,
     ]:  # 33
-        for m in [1, 2, 3]:
+        for m in [2]:
             g = nx.barabasi_albert_graph(n=n, m=m, seed=seed)
             graphs[f"barabasi_albert_{n}_{m}_{seed}"] = g
 

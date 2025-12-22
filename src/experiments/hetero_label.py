6f75671ffd68b30d263f40d831c4a9ff782fdbc1
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2025-12-22 18:34:25 +0000

    reduced number of heterogenous experiments

--- src/experiments/hetero_label.py
+++ src/experiments/hetero_label.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
         ]:
             wd = 0
             num_example = 5000
-            checkpoint_every = 10
+            checkpoint_every = 2
             task_type = "multiply"
             if data == "tiny_mem":
                 num_example = 33000
@@ -88,79 +88,80 @@ if __name__ == "__main__":
                 optimizer = "sgd"
                 # optimizer = "adam"
             # for softmax_coeff in [10, 100]:
-            for label_alpha in [1, 10, 1000]:
-                for sample_alpha in [1, 10, 1000]:
-                    for softmax_coeff in [10]:
-                        # for softmax_coeff in [2, 4, 6, 8, 10, 100]:
-                        # iterate through aggregation strategies
-                        for aggregation_strategy in [
-                            "unweighted",
-                            "weighted",
-                            "unweighted_fl",
-                            "random",
-                            "degCent",
-                            "betCent",
-                            # "degCent_sim",
-                            # "betCent_sim",
-                        ]:
-                            for scheduler in [None]:  # , "exp", "CA"]:
-                                for eta_min in [
-                                    1,
-                                ]:  # 0, -5, -10]:
-                                    for T_0 in [66]:  # 5, 8, 10]:
-                                        # iterate through topologies
-                                        for topo, node_set in zip(paths, nodes):
-                                            if "33" not in topo:
-                                                continue
-                                            # iterate through different backdoor node placements
-                                            # print(f"{topo=}, {node_set=}")
-                                            topology = np.loadtxt(topo, dtype=float)
-                                            num_clients = topology.shape[0]
+            for label_alpha, sample_alpha in zip(
+                [1, 10, 1000, 1000, 1000], [1000, 1000, 1000, 10, 1]
+            ):
+                for softmax_coeff in [10]:
+                    # for softmax_coeff in [2, 4, 6, 8, 10, 100]:
+                    # iterate through aggregation strategies
+                    for aggregation_strategy in [
+                        "unweighted",
+                        "weighted",
+                        "unweighted_fl",
+                        "random",
+                        "degCent",
+                        "betCent",
+                        # "degCent_sim",
+                        # "betCent_sim",
+                    ]:
+                        for scheduler in [None]:  # , "exp", "CA"]:
+                            for eta_min in [
+                                1,
+                            ]:  # 0, -5, -10]:
+                                for T_0 in [66]:  # 5, 8, 10]:
+                                    # iterate through topologies
+                                    for topo, node_set in zip(paths, nodes):
+                                        if "33" not in topo:
+                                            continue
+                                        # iterate through different backdoor node placements
+                                        # print(f"{topo=}, {node_set=}")
+                                        topology = np.loadtxt(topo, dtype=float)
+                                        num_clients = topology.shape[0]
 
-                                            if softmax_coeff != 10 and (
-                                                aggregation_strategy
-                                                in [
-                                                    "degCent",
-                                                    "betCent",
-                                                    "random",
-                                                    "unweighted",
-                                                    "weighted",
-                                                    "unweighted_fl",
-                                                ]
-                                            ):
-                                                continue
+                                        if softmax_coeff != 10 and (
+                                            aggregation_strategy
+                                            in [
+                                                "degCent",
+                                                "betCent",
+                                                "random",
+                                                "unweighted",
+                                                "weighted",
+                                                "unweighted_fl",
+                                            ]
+                                        ):
+                                            continue
 
-                                            num_experiments += 1
-                                            experiment_args = {
-                                                "dataset": data,
-                                                "rounds": args.rounds,
-                                                "topology_path": topo,
-                                                "backdoor": False,
-                                                "prox_coeff": 0,
-                                                "epochs": 5,
-                                                "aggregation_strategy": aggregation_strategy,
-                                                "log_dir": "hetero_logs",
-                                                "softmax": True,
-                                                "optimizer": optimizer,
-                                                "softmax_coeff": softmax_coeff,
-                                                "sample_alpha": sample_alpha,
-                                                "label_alpha": label_alpha,
-                                                "lr": lr,
-                                                "batch_size": 64,
-                                                "weight_decay": wd,
-                                                "beta_1": 0.9,
-                                                "beta_2": 0.98,
-                                                "n_layer": 1,
-                                                "task_type": task_type,
-                                                "num_example": num_example,
-                                                "checkpoint_every": checkpoint_every,
-                                                "tiny_mem_num_labels": 5,
-                                                "scheduler": scheduler,
-                                                "eta_min": eta_min,
-                                                "T_0": T_0,
-                                                "seed": seed,
-                                            }
-                                            param_list.append(experiment_args)
+                                        num_experiments += 1
+                                        experiment_args = {
+                                            "dataset": data,
+                                            "rounds": args.rounds,
+                                            "topology_path": topo,
+                                            "backdoor": False,
+                                            "prox_coeff": 0,
+                                            "epochs": 5,
+                                            "aggregation_strategy": aggregation_strategy,
+                                            "log_dir": "hetero_logs",
+                                            "softmax": True,
+                                            "optimizer": optimizer,
+                                            "softmax_coeff": softmax_coeff,
+                                            "sample_alpha": sample_alpha,
+                                            "label_alpha": label_alpha,
+                                            "lr": lr,
+                                            "batch_size": 64,
+                                            "weight_decay": wd,
+                                            "beta_1": 0.9,
+                                            "beta_2": 0.98,
+                                            "n_layer": 1,
+                                            "task_type": task_type,
+                                            "num_example": num_example,
+                                            "checkpoint_every": checkpoint_every,
+                                            "tiny_mem_num_labels": 5,
+                                            "scheduler": scheduler,
+                                            "eta_min": eta_min,
+                                            "T_0": T_0,
+                                            "seed": seed,
+                                        }
+                                        param_list.append(experiment_args)
 
     print(f"{num_experiments=}")
     ######### Parsl

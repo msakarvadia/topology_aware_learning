478bc3677b77c0c22a2250013e1503fbc1de0d05
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2026-01-30 01:30:28 +0000

    updating parsl set up w/ dummy experiment

--- src/experiments/parsl_setup.py
+++ src/experiments/parsl_setup.py
@@ -271,3 +271,35 @@ def run_experiment(machine_name="aurora", **kwargs):
     # parsl.dfk().cleanup()
     decentral_app.close()
     return exit_value
+
+
+@python_app(executors=["experiment"])
+def run_dummy_experiment(machine_name="aurora", **kwargs):
+    from src.dummy_decentralized_app import DummyDecentrallearnApp
+
+    ### Parsl set up     import parsl
+    from src.experiments.parsl_setup import get_parsl_config
+
+    experiment_config = "aurora_single_experiment"
+    if "polaris" in machine_name:
+        experiment_config = "polaris_single_experiment"
+    config, num_accelerators = get_parsl_config(experiment_config)
+    try:
+        # might have error loading config if parsl
+        # session from prior experiment isn't killed properly
+        parsl.load(config)
+    except:
+        print("parsl config already loaded")
+        # return 1
+    ### Parsl set up
+
+    decentral_app = DummyDecentrallearnApp()
+    # NOTE(MS): this is my attempt to handle run failures
+    # And to ensure parsl cleans up even if app doesn't successfully run
+    try:
+        exit_value = decentral_app.run()
+    except:
+        exit_value = 1
+    # parsl.dfk().cleanup()
+    decentral_app.close()
+    return exit_value
